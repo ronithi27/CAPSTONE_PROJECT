2322a5dc4d7596f28342b9a9a4322db6ed986a5b
Ronithi2005 <ronit.singh2024@nst.rishihood.edu.in>
2025-12-03 15:06:21 +0530

    Fix CORS: allow x-clerk-user-id header

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.use(cors({
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'x-user-id']
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-user-id', 'x-clerk-user-id']
 }));
 app.use(express.json());
 
